961a534447c39385e3ae7cb7300627ae1c78b66e
Nikhil Yadav <nickelyadav57@gmail.com>
2023-12-21 10:31:02 +0530

    :tada: init: Initializes project

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Nikhil Yadav on 19/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
